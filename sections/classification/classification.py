15fdcd260986e454eb1f79e212d2bff9f19d8898
mkunegel <melissa.kunegel@gmail.com>
2024-09-05 16:25:01 +0200

    J02 séparaion des pages et dataImportFini

--- sections/classification/classification.py
+++ sections/classification/classification.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def classification_page():
     st.header("Bienvenue")
-    st.caption("Bienvenue dans la classification")
+    st.caption("Bienvenue dans le Playground de classification")
--- sections/regression/regression.py
+++ sections/regression/regression.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def regression_page():
     st.header("Bienvenue")
-    st.caption("Bienvenue dans le Playground de Regression")
+    st.caption("Bienvenue dans le Playground de regression")
